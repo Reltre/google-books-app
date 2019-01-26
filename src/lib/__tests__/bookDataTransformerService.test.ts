6dcd5e14e6633dc78da1cf3568d7f87fe59d6a5a
Reltre <tglippert@gmail.com>
2019-01-25 23:48:18 -0800

    Ensure correct shape of book data in book transformer class.

--- src/lib/__tests__/bookDataTransformerService.test.ts
+++ src/lib/__tests__/bookDataTransformerService.test.ts
@@ -1,20 +1,31 @@
-import { BookDataTransformer, BookInfo }from '../bookDataTransformerService'
+import { BookDataTransformer, BookInfo, IBookInfo }from '../bookDataTransformerService'
+import { sampleBookData } from '../fixtures/sampleBookData'
 
+const details: IBookInfo = {
+  authors: sampleBookData.items[0].volumeInfo.authors,
+  title: sampleBookData.items[0].volumeInfo.title,
+  publisher: sampleBookData.items[0].volumeInfo.publisher,
+  image: sampleBookData.items[0].volumeInfo.imageLinks.thumbnail,
+  url: sampleBookData.items[0].volumeInfo.infoLink
+}
 
 test("book info contains an author property", () => {
-  const info = new BookInfo()
-  expect(info).toHaveProperty('author')
+  const info = new BookInfo(details)
+  expect(info).toHaveProperty('authors')
 })
 test("book info contains a title property", () => {
-  const info = new BookInfo()
+  const info = new BookInfo(details)
   expect(info).toHaveProperty('title')
 })
 test("book info contains a publishingCompany property", () => {
-  const info = new BookInfo()
-  expect(info).toHaveProperty('publishingCompany')
+  const info = new BookInfo(details)
+  expect(info).toHaveProperty('publisher')
 })
 test("book info contains an image property", () => {
-  const info = new BookInfo()
+  const info = new BookInfo(details)
   expect(info).toHaveProperty('image')
 })
-test('book has property url')
+test('book has property url', () => {
+  const info = new BookInfo(details)
+  expect(info).toHaveProperty('url')
+})
--- src/lib/bookDataTransformerService.ts
+++ src/lib/bookDataTransformerService.ts
@@ -1,6 +1,27 @@
-interface IBookInfo {}
+import { url } from "inspector";
+
+export interface IBookInfo {
+  authors: string[]
+  title: string
+  publisher: string
+  image: string
+  url: string
+}
 
 export class BookInfo implements IBookInfo {
+  authors: string[]
+  title: string
+  publisher: string
+  image: string
+  url: string
+
+  constructor({authors, title, publisher, image, url}: IBookInfo) {
+    this.authors = authors
+    this.title = title
+    this.publisher = publisher
+    this.image = image
+    this.url = url
+  }
 }
 
 export class BookDataTransformer {
