d5bd4c2527fc5d6021c030f3d346a7f48bf60681
Reltre <tglippert@gmail.com>
2019-01-27 21:30:38 -0800

    Create view helper for listing book authors.

--- src/controllers/searchController.ts
+++ src/controllers/searchController.ts
@@ -1,11 +1,28 @@
 import { BookRetrieval } from '../lib/bookRetrievalService'
 import { BookDataTransformer } from '../lib/bookDataTransformerService';
 export class SearchController {
+  static helpers = {
+    listFormat: (authors: Array<string>) => {
+      if (authors.length === 2) {
+        return authors.join(" and ")
+      } else if (authors.length > 2) {
+        const allButLastAuthor = authors.slice(0, -1).join(", ")
+        const conjunction = "and "
+        return allButLastAuthor.concat(conjunction).concat(authors[-1])
+      } else {
+        return authors[0]
+      }
+    }
+  }
+
   async search(req: any, res: any) {
     if(req.query.search_term) {
       const rawData = await BookRetrieval.search(req.query.search_term)
       const bookData = await BookDataTransformer.parse(rawData)
-      res.status(200).render('index', {books: bookData});
+      res.status(200).render(
+        'index', 
+        {books: bookData, listFormat: SearchController.helpers.listFormat}
+      );
     } else {
       res.status(200).render('index', {books: []});
     }
