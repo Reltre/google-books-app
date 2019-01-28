01b92b6ef1495212f53bda00871e723be687b54b
Reltre <tglippert@gmail.com>
2019-01-27 20:16:45 -0800

    Add conditional to determine correct response for root path

--- src/controllers/searchController.ts
+++ src/controllers/searchController.ts
@@ -2,8 +2,12 @@ import { BookRetrieval } from '../lib/bookRetrievalService'
 import { BookDataTransformer } from '../lib/bookDataTransformerService';
 export class SearchController {
   async search(req: any, res: any) {
-    const rawData = await BookRetrieval.search(req.query.search_term)
-    const bookData = await BookDataTransformer.parse(rawData)
-    res.status(200).send(JSON.stringify(bookData));
+    if(req.query.search_term) {
+      const rawData = await BookRetrieval.search(req.query.search_term)
+      const bookData = await BookDataTransformer.parse(rawData)
+      res.status(200).render('index', {books: bookData});
+    } else {
+      res.status(200).render('index', {books: []});
+    }
   }
 }
