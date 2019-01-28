f98775a71d12ce028caca298cc3c63d503c6b1af
Reltre <tglippert@gmail.com>
2019-01-27 20:15:53 -0800

    Setup static assets for application

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,10 @@
 import * as express from 'express'
 import SearchRouter from './routes/search'
+var path = require('path')
 
 const app = express()
 app.set('view engine', 'pug')
+app.set('views', './src/views')
+app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', SearchRouter)
 export default app
